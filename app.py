74987c27dbff5038c4d7c75d586def79b3cbb127
amrendra01 <singhamrendra1999@gmail.com>
2022-07-09 12:35:30 +0530

    update host

--- app.py
+++ app.py
@@ -493,5 +493,5 @@ def about():
     return render_template("about.html")
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
